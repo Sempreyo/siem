0dce403f6f291ae4a2a21a816bdd8085c415d188
Sempreyo <snickgem@gmail.com>
2025-12-17 18:59:23 +0300

    adaptive homepage

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,43 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const header = document.querySelector(".header");
+	const sectionHero = document.querySelector(".hero");
+	const sectionAdvantages = document.querySelector(".advantages");
 	const video = document.querySelector(".video video");
 	const videoPoster = document.querySelector(".video .video__poster");
 
 	if (video) {
 		videoPoster.addEventListener("click", (e) => {
-			e.currentTarget.classList.add("hide");
+			e.currentTarget.setAttribute("hidden", "true");
 			video.play();
 		});
 	}
 
-	if (header) {
+	const animateSection = (section, cb) => {
+		if (section) {
+			const observerSection = new IntersectionObserver((entries, observer) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						section.classList.remove("animate");
+
+						if (cb != undefined) {
+							cb();
+						}
+					}
+				});
+			}, { threshold: 0.2 });
+
+			observerSection.observe(section);
+		}
+	}
+
+	animateSection(sectionHero, () => {
 		/* Разделить строку на символы */
 		const splitText = new SplitType(".split-title", {
 			types: "chars"
 		});
-		const titleShadow = document.querySelector(".header__title-shadow img");
 
-		document.querySelector(".split-title").classList.add("visible");
+		sectionHero.querySelector(".split-title").classList.add("visible");
+		const chars = sectionHero.querySelectorAll(".split-title .char");
 
-		/* Анимация появления символов в первом блоке */
-		const heroObserver = new IntersectionObserver((entries, observer) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					const chars = document.querySelectorAll(".split-title .char");
-
-					chars.forEach((el, index) => {
-						setTimeout(() => {
-							el.style.transform = 'translateX(0)';
-						}, 50 * index);
-					});
-
-					setTimeout(() => {
-						titleShadow.classList.add("visible");
-					}, 50 * chars.length + 300);
-				}
-			});
+		chars.forEach((el, index) => {
+			setTimeout(() => {
+				el.style.transform = 'translateX(0)';
+			}, 50 * index + 700);
 		});
+	});
 
-		heroObserver.observe(header);
-	}
+	animateSection(sectionAdvantages);
 });
